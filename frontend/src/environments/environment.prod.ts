0eae44181786f72454bbba3aa509a86cad5e2d50
ANAND KASAUDHAN <anandkota9151@gmail.com>
2026-01-02 18:42:19 +0530

    backend is connected

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'YOUR_RENDER_BACKEND_URL/api/',
-    uploadUrl: 'YOUR_RENDER_BACKEND_URL/uploads/',
+    apiUrl: 'https://healthconnect-backend-eg82.onrender.com/api',
+    uploadUrl: 'https://healthconnect-backend-eg82.onrender.com/uploads',
     googleMapsApiKey: 'AIzaSyBRzFATDzlAVWOLgXg0fcSXwt54PjcjCB4'
 };
